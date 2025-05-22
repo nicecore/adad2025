4a798bcbe521fc9aeb353ff91e3f8e0456acba93
Nicecore <23281721+nicecore@users.noreply.github.com>
2025-05-22 18:32:47 -0500

    test4

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,3 +1,4 @@
+import { HtmlBasePlugin } from "@11ty/eleventy";
 
 function blogDate(input) {
   return `${new Date(input).toLocaleString("en-US", {
@@ -10,7 +11,10 @@ function blogDate(input) {
 
 export default async function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
-    eleventyConfig.addFilter('blogDate', blogDate)
+    eleventyConfig.addFilter('blogDate', blogDate);
+    eleventyConfig.addPlugin(HtmlBasePlugin, {
+      baseHref: "/adad2025/"
+    });
   }
 
 
